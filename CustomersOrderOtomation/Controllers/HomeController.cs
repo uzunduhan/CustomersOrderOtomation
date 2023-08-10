e18570cf442b7f0e20ca73d394851d623e8b3edf
Duhan Uzun <duhan181999@gmail.com>
2023-08-10 21:18:35 +0300

    kategoriye göre ürün getirme ve ürün detaylarını getirme işlemleri yapıldı

--- CustomersOrderOtomation/Controllers/HomeController.cs
+++ CustomersOrderOtomation/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
-﻿using CustomersOrderOtomation.Service.Abstract;
+﻿using AutoMapper;
+using CustomersOrderOtomation.Data.DBOperations;
+using CustomersOrderOtomation.Service.Abstract;
 using CustomersOrderOtomation.ViewModel.Category;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CustomersOrderOtomation.Controllers
 {
@@ -9,12 +12,16 @@ namespace CustomersOrderOtomation.Controllers
         private readonly ILogger<HomeController> _logger;
         private readonly ICategoryService categoryService;
         private readonly IProductService productService;
+        private readonly DatabaseContext context;
+        private readonly IMapper mapper;
 
-        public HomeController(ILogger<HomeController> logger, ICategoryService categoryService, IProductService productService)
+        public HomeController(ILogger<HomeController> logger, ICategoryService categoryService, IProductService productService, DatabaseContext context, IMapper mapper)
         {
             _logger = logger;
             this.categoryService = categoryService;
             this.productService = productService;
+            this.context = context;
+            this.mapper = mapper;
         }
 
         public IActionResult Index()
@@ -27,21 +34,31 @@ namespace CustomersOrderOtomation.Controllers
             return View();
         }
 
-        public async Task <IActionResult> Shop()
+        public async Task<IActionResult> Shop()
         {
             var products = await productService.GetAllProducts();
             return View(products);
         }
 
-        public IActionResult ShopDetailed(int productId)
+        public async Task<IActionResult> ShopDetailed(int productId)
         {
-            return View();
+            var produt = await productService.GetSingleProductByIdAsync(productId);
+            return View(produt);
         }
 
         public async Task<List<CategoryViewModel>> GetCategories()
         {
             return await categoryService.GetAllCategories();
         }
+
+        public async Task<IActionResult> GetProductsByCategory(int categoryId)
+        {
+            var products = await context.ProductCategories.Where(x => x.CategoryId == categoryId).Select(x => x.Product).ToListAsync();
+            List<ViewModel.Product.ProductViewModel> vm = mapper.Map<List<ViewModel.Product.ProductViewModel>>(products);
+            return View("Shop", vm);
+        }
+
+
 
     }
 }
