9ca2775f62e26edc18ac0a915419423c36c3b092
Duhan Uzun <duhan181999@gmail.com>
2023-08-16 22:41:02 +0300

    Admin menüsü ayarlandı, ürünleri düzenlemek için taslak oluşturuldu

---
+++ CustomersOrderOtomation/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CustomersOrderOtomation.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AdminProduct()
+        {
+            return View();
+        }
+    }
+}
